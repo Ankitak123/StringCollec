46f719f186699e2a7caebf6cdf8404f140b9bbd0
Ankesh <Ankesh@LENOVO>
2021-08-30 20:34:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StringCollec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays Sample!");
+            // Construct a dynamic array and initialize with 5 strings  
+            string[] strArray = new string[] { "Java", "Python", "C++", "C#", "C" };
+            // Display array items  
+            Console.WriteLine(strArray[0]);
+            Console.WriteLine(strArray[1]);
+            Console.WriteLine(strArray[2]);
+            Console.WriteLine(strArray[3]);
+            Console.WriteLine(strArray[4]);
+            Console.ReadKey();
+        }
+    }
+}
+    
+
